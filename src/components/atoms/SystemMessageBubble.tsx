70da2436428e107f3e6c353ed77e3e356a8f3dac
nrgould <nrgould197@gmail.com>
2024-12-03 14:39:54 -0500

    animated message bubbles

--- src/components/atoms/SystemMessageBubble.tsx
+++ src/components/atoms/SystemMessageBubble.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Label } from '../ui/label';
+import { motion } from 'framer-motion';
 interface Props {
 	message: string;
 }
 
 const SystemMessage = ({ message }: Props) => {
 	return (
-		<div className='flex justify-start items-start'>
+		<motion.div
+			className='flex justify-start items-start'
+			initial={{ opacity: 0, y: 10 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.1, ease: 'easeIn' }}
+		>
 			<div className='flex flex-col items-start'>
 				<Label>System</Label>
 				<p className='py-2 max-w-xs md:max-w-md lg:max-w-[40rem] rounded-2xl text-black leading-[2rem]'>
 					{message}
 				</p>
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
--- src/components/atoms/UserMessageBubble.tsx
+++ src/components/atoms/UserMessageBubble.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Label } from '../ui/label';
+import { motion } from 'framer-motion';
 
 interface Props {
 	message: string;
@@ -7,11 +7,16 @@ interface Props {
 
 const UserMessage = ({ message }: Props) => {
 	return (
-		<div className='flex justify-end items-start space-x-2'>
+		<motion.div
+			className='flex justify-end items-start space-x-2'
+			initial={{ opacity: 0, y: 10 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.1, ease: 'easeIn' }}
+		>
 			<div className='px-4 py-2 max-w-xs md:max-w-lg lg:max-w-[50rem] rounded-2xl bg-black text-white'>
 				{message}
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
