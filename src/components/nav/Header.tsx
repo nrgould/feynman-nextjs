cef305324e06447b4aad6cf693cbe65447d9f996
nrgould <nrgould197@gmail.com>
2024-12-20 16:21:13 -0500

    auth buttons in header

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -5,20 +5,42 @@ import { SidebarTrigger } from '../ui/sidebar';
 import { ChevronLeft } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { Button } from '../ui/button';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { Skeleton } from '../ui/skeleton';
 
 function Header() {
 	const pathname = usePathname();
+	const { user, isLoading } = useUser();
 
 	return (
-		<header className='sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4 bg-white z-50 h-[3rem]'>
-			{pathname !== '/' && (
-				<Link href='..' className='no-icon'>
-					<ChevronLeft size={20} />
-				</Link>
-			)}
-			<SidebarTrigger className='-ml-1' />
+		<header className='sticky top-0 flex shrink-0 items-center justify-between gap-2 border-b bg-background p-4 bg-white z-50 h-[3rem]'>
 			<div className='flex flex-row items-center justify-between'>
-				<h1 className='font-bold text-xl'>Feynman Learning</h1>
+				{/* {pathname !== '/' && (
+					<Link href='..' className='no-icon'>
+						<ChevronLeft size={20} />
+					</Link>
+				)} */}
+				<SidebarTrigger className='-ml-1 mr-2' />
+				<div className='flex flex-row items-center justify-between'>
+					<h1 className='font-bold text-xl'>Feynman Learning</h1>
+				</div>
+			</div>
+			<div className='flex flex-row items-center justify-between gap-2'>
+				{!user && !isLoading && (
+					<div className='flex flex-row items-center justify-between gap-2'>
+						<Link href='/api/auth/login'>
+							<Button variant='outline'>Login</Button>
+						</Link>
+						<Button variant='default'>Sign Up</Button>
+					</div>
+				)}
+				{isLoading && (
+					<div className='flex flex-row items-center justify-between gap-2'>
+						<Skeleton className='h-8 w-16' />
+						<Skeleton className='h-8 w-20' />
+					</div>
+				)}
 			</div>
 		</header>
 	);
