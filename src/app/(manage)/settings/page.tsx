33ffb0c5ea5e098f15f06938a4cdfa6303cf8ca5
nrgould <nrgould197@gmail.com>
2025-01-07 18:08:18 -0500

    settings tabs

--- src/app/(manage)/settings/page.tsx
+++ src/app/(manage)/settings/page.tsx
@@ -4,6 +4,7 @@ import { getSession } from '@auth0/nextjs-auth0';
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 import { getUserProfile } from './actions';
+import { Tabs, TabsTrigger, TabsList, TabsContent } from '@/components/ui/tabs';
 
 export default async function SettingsPage() {
 	const session = await getSession();
@@ -16,9 +17,26 @@ export default async function SettingsPage() {
 
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
-			<div className='w-full flex flex-col items-start justify-between p-4 mx-auto md:w-1/2 lg:w-1/3'>
-				<Subtitle>Settings</Subtitle>
-				<ProfileSettings initialData={userData} />
+			<Subtitle className='pt-4 pl-4 font-bold mb-0'>Settings</Subtitle>
+			<div className='w-full p-4 mx-auto flex flex-col items-center justify-center'>
+				<Tabs defaultValue='account' className='w-[600px]'>
+					<TabsList className='grid w-full grid-cols-3'>
+						<TabsTrigger value='profile'>Profile</TabsTrigger>
+						<TabsTrigger value='account'>Account</TabsTrigger>
+						<TabsTrigger value='billing'>Billing</TabsTrigger>
+					</TabsList>
+					<TabsContent value='profile'>
+						<div className='w-full flex flex-col items-start justify-between p-4 mx-auto'>
+							<ProfileSettings initialData={userData} />
+						</div>
+					</TabsContent>
+					<TabsContent value='account'>
+						<h2>Account</h2>
+					</TabsContent>
+					<TabsContent value='billing'>
+						<h2>Billing</h2>
+					</TabsContent>
+				</Tabs>
 			</div>
 		</Suspense>
 	);
