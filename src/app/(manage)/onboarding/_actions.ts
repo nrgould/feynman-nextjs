560b198c2e888820b0263902fbf583e5356c33b6
nrgould <nrgould197@gmail.com>
2025-01-20 13:04:09 -0500

    fixed onboarding flow

--- src/app/(manage)/onboarding/_actions.ts
+++ src/app/(manage)/onboarding/_actions.ts
@@ -1,10 +1,12 @@
 'use server';
 
-import { createUser } from '@/lib/db/queries';
+import { createClient } from '@/utils/supabase/server';
 import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 
 export const completeOnboarding = async (formData: FormData) => {
 	const client = await clerkClient();
+	const supabase = await createClient();
+
 	const { userId } = await auth();
 	const user = await currentUser();
 
@@ -12,22 +14,47 @@ export const completeOnboarding = async (formData: FormData) => {
 		return { message: 'No Logged In User' };
 	}
 
+	console.log('user: ', user);
+
 	try {
-        await client.users.updateUser(userId, {
+		// // Update Clerk user metadata
+		await client.users.updateUser(userId, {
 			publicMetadata: {
 				onboardingComplete: true,
 			},
 		});
-		await createUser({
-			...formData,
-			userId,
-			email: user.emailAddresses[0].emailAddress,
-			username: user.username || '',
-			name: user.firstName + ' ' + user.lastName,
-		});
-		return { message: 'User metadata Updated' };
+
+		// Insert user into Supabase
+		const { data, error } = await supabase
+			.from('User')
+			.insert({
+				id: userId,
+				name: user.firstName + ' ' + user.lastName || null,
+				username: user.username || null,
+				email: user.emailAddresses[0].emailAddress,
+				learning_disability:
+					(formData.get('learningDisability') as string) || null,
+				goals: (formData.get('goals') as string) || null,
+				selected_subjects: formData.get('selectedSubjects')
+					? (formData.get('selectedSubjects') as string).split(',')
+					: [],
+				profile_image: user.imageUrl || null,
+				referral_source:
+					(formData.get('referralSource') as string) || null,
+				education_level:
+					(formData.get('educationLevel') as string) || null,
+				account_type: 'free',
+				concept_limit: 3,
+				concepts: [],
+			})
+			.select()
+			.single();
+
+		if (error) throw error;
+
+		return { message: 'User created successfully', data };
 	} catch (e) {
-		console.log('error', e);
-		return { message: 'Error Updating User Metadata' };
+		console.error('Error in completeOnboarding:', e);
+		return { message: 'Error creating user', error: e };
 	}
 };
