f606f0a8938f5d1a9550b383ab3137192667565b
nrgould <nrgould197@gmail.com>
2025-02-19 16:55:37 -0500

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,5 +20,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			changeFrequency: 'monthly',
 			priority: 0.6,
 		},
+		{
+			url: 'https://www.feynlearn.com/feynman-technique',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 1,
+		},
 	];
 }
